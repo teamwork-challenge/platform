1614b213657b1f6e843c773e72042470f54be098
Pavel Egorov <pe.xoposhiy@gmail.com>
2025-08-20 22:42:18 +0200

    Reorganize for deploy

--- api_models/__init__.py
+++ api_models/__init__.py
@@ -1,2 +1,2 @@
-# Re-export all models from the inner package so consumers can use `from api_models import ...`
-from .api_models import *  # noqa: F401,F403
+from .models import *
+from .gen_models import *
